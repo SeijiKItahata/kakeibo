03f841a3d3513bf500a16f08f5a39b065b61a905
seiji <seijik49@gmail.com>
2025-10-05 15:47:56 +0900

    Update HomeServlet: add inital home page message

--- src/main/java/com/kakeibo/servlet/HomeServlet.java
+++ src/main/java/com/kakeibo/servlet/HomeServlet.java
@@ -8,10 +8,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class HomeServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html; charset=UTF-8");
-        response.getWriter().println("<h1>Hello from HelloServlet!</h1>");
-    }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	        throws ServletException, IOException {
+	    response.setContentType("text/html; charset=UTF-8");
+
+	    // 画面に表示するHTML
+	    response.getWriter().println("<h1>家計簿アプリへようこそ！</h1>");
+	    response.getWriter().println("<p>これは最初のホーム画面です。</p>");
+	}
+
 }
